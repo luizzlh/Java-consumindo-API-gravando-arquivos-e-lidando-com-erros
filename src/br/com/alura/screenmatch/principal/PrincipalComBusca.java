3cac38a7f7884410f9dde75d5395c765690ed416
Luiz <luizzlh2004@gmail.com>
2025-11-08 20:55:04 -0300

    Criação do cliente, da requisição, envio da requisição do cliente e coleta do resultado.

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -0,0 +1,28 @@
+package br.com.alura.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um filme para busca: ");
+        var busca = scanner.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=35ffd357";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
+    }
+}
